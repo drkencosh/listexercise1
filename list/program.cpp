0ae024ab70f3c1acd5ff30c53e7e07da493cc54d
drkencosh <drkencosh@gmail.com>
2014-07-28 13:05:00 +0700

    added line

--- list/program.cpp
+++ list/program.cpp
@@ -12,4 +12,5 @@ void main()
 	mylist.print();
 
 	//TO DO! Write a program that tests your list library - the code should take characters, push them onto a list, - then reverse the list to see if it is a palindrome!
+
 }
